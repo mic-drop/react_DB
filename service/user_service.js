9a452a9490119c902f4180f959a12c107791003f
mic_drop <miguel.saraiva@academiadecodigo.org>
2025-03-17 16:46:52 +0000

    adds user service

---
+++ service/user_service.js
@@ -0,0 +1,26 @@
+const User = require('../model/user')
+const Repository = require('../repository/repository.js')
+
+const internals = {
+    userRepo: new Repository(User)
+}
+
+exports.findById = async (id) => {
+    const user = await userRepo.findById(id);
+    return user;
+}
+
+exports.findAll = async () => {
+    const user = await userRepo.findAll();
+    return user;
+}
+
+exports.add = async (user) => {
+    const user = await userRepo.add(user);
+
+    return user.id;
+}
+
+exports.delete = async (id) => {
+    const id = await userRepo.delete(id);
+}
