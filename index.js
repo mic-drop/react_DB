e9238938974f750d6712018514dc3ef5341bdf69
mic_drop <miguel.saraiva@academiadecodigo.org>
2025-03-18 16:14:27 +0000

    adds knex config to the server

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const Hapi = require('@hapi/hapi');
-const { endpoints } = require('./routes'); // assuming your endpoints are exported here
+const { endpoints } = require('./routes'); 
+
+
+const Knex = require('knex');
+const knexConfig = require('./knexfile').test;
+const knexInstance = Knex(knexConfig);
 
 const init = async () => {
   const server = Hapi.server({
     host: 'localhost',
     port: 3000
   });
+
+  server.app.knex = knexInstance;
 
   // Register all endpoints from the array
   server.route(endpoints);
