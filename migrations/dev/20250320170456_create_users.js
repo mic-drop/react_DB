5448c5e3da792bb6dc608a219f13a306c4f4c325
mic_drop <miguel.saraiva@academiadecodigo.org>
2025-03-20 17:07:24 +0000

    adds dev seeds && migrations

---
+++ migrations/dev/20250320170456_create_users.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return Promise.all([
+        //user table
+        knex.schema.createTable('users', function(table){
+            table.increments().primary();
+            table.string('name');
+        })
+    ])
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTableIfExists('users')
+  ])
+};
--- seeds/dev/seed_users.js
+++ seeds/dev/seed_users.js
@@ -3,7 +3,7 @@ exports.seed = async function(knex) {
   
     await knex('users').insert([
       { id: 1, name: 'Mic'},
-      { id: 2, name: 'Drop'} 
+      { id: 2, name: 'Drop'},
+      { id: 3, name: 'Pete'}
     ]);
   };
-  
